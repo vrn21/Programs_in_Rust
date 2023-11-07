32a0e76f8fc2096f3ef3977cd149391de7377fc4
K V Varun Krishnan <varunkkv.mec@gmail.com>
2023-11-07 08:56:31 +0530

    Geuss game cmpleted, using Ok() and Err()

--- src/bin/geuss.rs
+++ src/bin/geuss.rs
@@ -1,8 +1,36 @@
+use core::num;
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main(){
-    println!("Enter a number");
-    let mut geuss = String::new();
-    io::stdin().read_line(&mut geuss).expect("Failed to read line");
-    println!("THe number you geussed is {}",geuss)
+    println!("Guessing Game !!!");
+    print!("Enter quit to quit the game:");
+        
+    let secret_number = rand::thread_rng().gen_range(1..=101);
+    loop {
+       println!("Enter your Number geuss, number is between 1 and 100");
+       let mut geuss = String::new();
+        io::stdin().read_line(&mut geuss).expect("Failed to read line");
+
+        let geuss: u32 = match geuss.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please type a number");
+                continue;
+            }
+        };
+        println!("you geussed: {}", geuss);
+
+        match geuss.cmp(&secret_number){
+            Ordering::Less => println!("The number input is smaller, than the secret number"),
+            Ordering::Greater => println!("The number input is greater, than the secret number"),
+            Ordering::Equal => {
+                println!("Yay! You geussed it right, You won!!");
+                break;
+            }
+        }
+    }
+    
+    
 }
---
+++ src/bin/temp.rs
@@ -0,0 +1,6 @@
+use std::io;
+
+fn main(){
+    let mut n=String::new();
+    io::stdin().read_line(&mut n).expect("This is an errr \n \n \n");
+}
